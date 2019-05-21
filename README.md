50913a37bbaf6762ff169ff0334a8dfbc908e5ea
Louis Thiery <louis@helium.com>
2019-05-21 11:59:31 -0700

    wip

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
 # helios
+
+So far only tested things on [STM32L0 Discovery kit](https://www.st.com/en/evaluation-tools/b-l072z-lrwan1.html), which features the [STM32L072CZ](https://www.st.com/en/microcontrollers-microprocessors/stm32l072cz.html).
+
+Run JLink server:
+`JLinkGDBServer -device STM32L072CZ -speed 4000 -if jtag -AutoConnect -1 -port 3333`
+
+If for some reason, you really want to use openocd:
+
+`sudo openocd -f ./openocd.cfg`
+
+At the moment, you need to restart the OpenOCD server every time you upload. The reason seems to be that if you try to flash without explicitly erasing, the flash fails. Starting the server clears the flash. JLink doesn't have this problem.
+
+Use a .cargo/config as such:
+
+```
+[target.thumbv6m-none-eabi]
+# uncomment this to make `cargo run` execute programs on QEMU
+# runner = "qemu-system-arm -cpu cortex-m3 -machine lm3s6965evb -nographic -semihosting-config enable=on,target=native -kernel"
+
+[target.'cfg(all(target_arch = "arm", target_os = "none"))']
+# uncomment ONE of these three option to make `cargo run` start a GDB session
+# which option to pick depends on your system
+# runner = "arm-none-eabi-gdb -q -x openocd.gdb"
+# runner = "gdb-multiarch -q -x openocd.gdb"
+# runner = "gdb -q -x openocd.gdb"
+runner = "gdb-multiarch -tui -q -x openocd.gdb"
+
+rustflags = [
+  # LLD (shipped with the Rust toolchain) is used as the default linker
+  "-C", "link-arg=-Tlink.x",
+
+  # if you run into problems with LLD switch to the GNU linker by commenting out
+  # this line
+  #"-C", "linker=arm-none-eabi-ld",
+
+  # if you need to link to pre-compiled C libraries provided by a C toolchain
+  # use GCC as the linker by commenting out both lines above and then
+  # uncommenting the three lines below
+  # "-C", "linker=arm-none-eabi-gcc",
+  # "-C", "link-arg=-Wl,-Tlink.x",
+  # "-C", "link-arg=-nostartfiles",
+]
+
+[build]
+# Pick ONE of these compilation targets
+target = "thumbv6m-none-eabi"    # Cortex-M0 and Cortex-M0+
+# target = "thumbv7m-none-eabi"    # Cortex-M3
+# target = "thumbv7em-none-eabi"   # Cortex-M4 and Cortex-M7 (no FPU)
+# target = "thumbv7em-none-eabihf" # Cortex-M4F and Cortex-M7F (with FPU)
+```
+
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,6 @@
-#![deny(warnings)]
-#![deny(unsafe_code)]
-#![no_main]
 #![no_std]
+#![no_main]
 #![feature(lang_items)]
-
-// panic handler
-extern crate panic_semihosting;
-
-use core::fmt::Write;
-use cortex_m_rt::entry;
-use sx1276;
-
-use stm32l0xx_hal::{pac, prelude::*, rcc::Config, serial, spi};
 
 //use nb::block;
 
@@ -37,55 +26,296 @@ use stm32l0xx_hal::{pac, prelude::*, rcc::Config, serial, spi};
 #define BOARD_TCXO_WAKEUP_TIME               5
 */
 
-#[entry]
-fn main() -> ! {
-    let dp = pac::Peripherals::take().unwrap();
+// panic handler
+extern crate panic_semihosting;
 
-    // Configure the clock.
-    let mut rcc = dp.RCC.freeze(Config::hsi16());
+use core::fmt::Write;
+use cortex_m_rt::entry;
+use sx1276;
+use stm32l0xx_hal as hal;
+use hal::{exti::TriggerEdge, gpio::*, pac, prelude::*, rcc::Config, serial, spi};
+use embedded_hal::digital::v2::OutputPin;
 
-    let gpioa = dp.GPIOA.split(&mut rcc);
+#[rtfm::app(device = stm32l0xx_hal::pac)]
+const APP: () = {
+    static mut LED: gpiob::PB5<Output<PushPull>> = ();
+    static mut INT: pac::EXTI = ();
+    static mut BUTTON: gpiob::PB2<Input<PullUp>> = ();
 
-    let tx_pin = gpioa.pa9;
-    let rx_pin = gpioa.pa10;
+    #[init]
+    fn init() -> init::LateResources {
 
-    let serial = dp
-        .USART1
-        .usart((tx_pin, rx_pin), serial::Config::default(), &mut rcc)
-        .unwrap();
+        let dp = pac::Peripherals::take().unwrap();
 
-    let (mut tx, _rx) = serial.split();
+        // Configure the clock.
+        let mut rcc = dp.RCC.freeze(Config::hsi16());
 
-    write!(tx, "Hello, world!\r\n").unwrap();
+        let gpioa = dp.GPIOA.split(&mut rcc);
 
-    let gpiob = dp.GPIOB.split(&mut rcc);
-    let sck = gpiob.pb3;
-    let miso = gpioa.pa6;
-    let mosi = gpioa.pa7;
-    let mut nss = gpioa.pa15.into_push_pull_output();
+        let tx_pin = gpioa.pa2;
+        let rx_pin = gpioa.pa3;
 
-    nss.set_high();
+        let serial = dp
+            .USART2
+            .usart((tx_pin, rx_pin), serial::Config::default(), &mut rcc)
+            .unwrap();
 
-    // Initialise the SPI peripheral.
-    let mut spi = dp
-        .SPI1
-        .spi((sck, miso, mosi), spi::MODE_0, 100_000.hz(), &mut rcc);
+        let gpiob = dp.GPIOB.split(&mut rcc);
+        let sck = gpiob.pb3;
+        let miso = gpioa.pa6;
+        let mosi = gpioa.pa7;
+        let nss = gpioa.pa15.into_push_pull_output();
 
-    radio = sx1276::Sx1276::new(spi, nss);
+        // Initialise the SPI peripheral.
+        let spi = dp
+            .SPI1
+            .spi((sck, miso, mosi), spi::MODE_0, 100_000.hz(), &mut rcc);
 
-    write!(tx, "{} reg x{:x}\r\n",0x06, radio.read(0x06)).unwrap();
+        // Acquire the GPIOB peripheral. This also enables the clock for GPIOB in
+        // the RCC register.
+        let gpiob = device.GPIOB.split(&mut rcc);
+
+        // Configure PB5 as output.
+        let led = gpiob.pb5.into_push_pull_output();
+
+        // Configure PB2 as input.
+        let button = gpiob.pb2.into_pull_up_input();
+
+        let exti = device.EXTI;
+        // Configure the external interrupt on the falling edge for the pin 2.
+        exti.listen(
+            &mut rcc,
+            &mut device.SYSCFG_COMP,
+            button.port,
+            button.i,
+            TriggerEdge::Falling,
+        );
+
+        // Return the initialised resources.
+        init::LateResources {
+            LED: led,
+            INT: exti,
+            BUTTON: button,
+        }
+
+    }
+
+    #[idle]
+    fn idle() -> !{
+        loop {}
+    }
+
+    #[interrupt(resources = [LED, INT, BUTTON])]
+    fn EXTI2_3() {
+        static mut STATE: bool = false;
+
+        // Clear the interrupt flag.
+        resources.INT.clear_irq(resources.BUTTON.i);
+        if *STATE {
+           resources.LED.set_low().unwrap();
+           *STATE = false;
+        } else {
+            resources.LED.set_high().unwrap();
+           *STATE = true;
+        }
+        
+    }
+
+};
+
+use stm32l0xx_hal::gpio::gpioa::*;
+use stm32l0xx_hal::gpio::gpiob::*;
+use stm32l0xx_hal::gpio::{Floating, Input, PushPull};
+use stm32l0xx_hal::pac::SPI1;
+
+use embedded_hal::spi::FullDuplex;
+use core::ffi; 
+use nb::block;
+
+
+#[repr(C, align(4))]
+pub struct SpiInstance {
+    Instance:*mut ffi::c_void,
+    //Reset: &Gpio_t,
+}
+
+#[repr(C, align(4))]
+pub struct Spi_s {
+    Spi: SpiInstance,
+    Nss: Gpio_t,
+}
+
+pub type Spi_t = Spi_s;
+
+
+//find more elegant way to make cbindgen export Spi_t
+#[no_mangle]
+pub extern "C" fn foo(s: Spi_t) {
+}
+
+#[no_mangle]
+pub extern "C" fn SpiInOut(s: &mut Spi_t, outData: u16) -> u16 {
+
+    let spi: &mut hal::spi::Spi<
+        SPI1,
+        (
+            PA3<Input<Floating>>,
+            PA6<Input<Floating>>,
+            PA7<Input<Floating>>,
+        ),
+    > = unsafe {
+        &mut *(s.Spi.Instance as *mut hal::spi::Spi<
+            SPI1,
+            (
+                PA3<Input<Floating>>,
+                PA6<Input<Floating>>,
+                PA7<Input<Floating>>,
+            ),
+        >)
+    };
+
+    /*
+    let spi: &mut hal::spi::Spi<
+        SPI1,
+        (
+            PB5<Input<Floating>>,
+            PA11<Input<Floating>>,
+            PA12<Input<Floating>>,
+        ),
+    > = unsafe {
+        &mut *(s.Spi.Instance as *mut hal::spi::Spi<
+            SPI1,
+            (
+                PB5<Input<Floating>>,
+                PA11<Input<Floating>>,
+                PA12<Input<Floating>>,
+            ),
+        >)
+    };
+    */
+
+
+    spi.send(outData as u8).unwrap();
+    let inData = block!(spi.read()).unwrap();
+
+    inData as u16
+}
+
+
+type Gpio_t = *mut ffi::c_void;
+
+#[repr(C)]
+pub enum PinNames {
+    MCU_PINS,
+    IOE_PINS,
+    RADIO_RESET,
+}
+
+#[repr(C)]
+pub enum PinModes {
+    PIN_INPUT = 0,
+    PIN_OUTPUT,
+    PIN_ALTERNATE_FCT,
+    PIN_ANALOGIC,
+}
+
+#[repr(C)]
+pub enum PinTypes {
+    PIN_NO_PULL = 0,
+    PIN_PULL_UP,
+    PIN_PULL_DOWN,
+}
+
+#[repr(C)]
+pub enum PinConfigs {
+    PIN_PUSH_PULL = 0,
+    PIN_OPEN_DRAIN,
+}
+
+
+#[no_mangle]
+pub extern "C" fn GpioInit(
+    obj: &Gpio_t,
+    pin: PinNames,
+    mode: PinModes,
+    config: PinConfigs,
+    pin_type: PinTypes,
+    val: u32,
+) {
 
 }
 
-/*
-uint8_t data;
+#[no_mangle]
+pub extern "C" fn GpioWrite(obj: Gpio_t, val: u8) {
+    //let gpio: &mut stm32l0xx_hal::gpio::gpioa::PA2<Output<PushPull>> =
+    //    unsafe { &mut *(obj as *mut stm32l0xx_hal::gpio::gpioa::PA2<Output<PushPull>>) };
+    let gpio: &mut stm32l0xx_hal::gpio::gpioa::PA15<Output<PushPull>> =
+        unsafe { &mut *(obj as *mut stm32l0xx_hal::gpio::gpioa::PA15<Output<PushPull>>) };
 
-SX1276Acquire( );
 
-stm32l0_gpio_pin_write(RADIO_NSS, 0);
+    if (val == 0) {
+        embedded_hal::digital::v2::OutputPin::set_low(gpio).unwrap();
+    } else {
+        embedded_hal::digital::v2::OutputPin::set_high(gpio).unwrap();
+    }
+}
 
-stm32l0_spi_data(&RADIO_SPI, addr & ~0x80);
-data = stm32l0_spi_data(&RADIO_SPI, 0xff);
+#[repr(C, align(4))]
+pub struct TimerEvent_s {
+    IsRunning: bool,
+}
 
-stm32l0_gpio_pin_write(RADIO_NSS, 1);
-*/
+type TimerEvent_t = TimerEvent_s;
+
+#[no_mangle]
+pub extern "C" fn TimerInit(obj: &TimerEvent_t, cb: Option<extern "C" fn()>) {}
+
+#[no_mangle]
+pub extern "C" fn TimerStart(obj: &TimerEvent_t) {}
+
+#[no_mangle]
+pub extern "C" fn TimerStop(obj: &TimerEvent_t) {}
+
+#[no_mangle]
+pub extern "C" fn TimerReset(obj: &TimerEvent_t) {}
+
+#[no_mangle]
+pub extern "C" fn TimerSetValue(obj: &TimerEvent_t, value: u32) {}
+
+#[no_mangle]
+pub extern "C" fn TimerGetCurrentTime() {}
+
+#[no_mangle]
+pub extern "C" fn TimerGetElapsedTime(saved_time: &TimerEvent_t) {}
+
+#[no_mangle]
+pub extern "C" fn TimerGetFutureTime(event_in_future: &TimerEvent_t) {}
+
+#[no_mangle]
+pub extern "C" fn TimerLowPowerHandler() {}
+
+type irq_ptr = extern "C" fn();
+
+#[no_mangle]
+pub extern "C" fn SX1276IoIrqInit(irq_handlers: [irq_ptr; 6]) {}
+
+#[no_mangle]
+pub extern "C" fn SX1276GetPaSelect(channel: u32) -> u8 {0}
+
+#[no_mangle]
+pub extern "C" fn DelayMs(ms: u32){
+
+    
+}
+
+#[no_mangle]
+pub extern "C" fn memcpy1(dst: &u8, src: &u8, size: u16){}
+
+#[no_mangle]
+pub extern "C" fn SX1276SetAntSwLowPower(status: bool){}
+
+#[no_mangle]
+pub extern "C" fn SX1276SetAntSw(rxTx: u8){}
+
+#[no_mangle]
+pub extern "C" fn assert_param(expr: bool){}
